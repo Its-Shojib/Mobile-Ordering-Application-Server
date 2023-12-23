00bbbd92f2539f730e5b3fc0686b7d492f072397
Its_Shojib <mdshojib922@gmail.com>
2023-12-23 11:55:40 +0600

    Vercel Deployed Successfully

--- index.js
+++ index.js
@@ -42,9 +42,6 @@ async function run() {
       res.send(result);
     })
 
-
-
-
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
